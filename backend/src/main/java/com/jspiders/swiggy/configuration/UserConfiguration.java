3059f601fb3d71a757d5df42521d413af7e75d1b
Srithar05 <sritharspk05@gmail.com>
2026-07-25 17:04:59 +0530

    Vercel Configuration added

--- backend/src/main/java/com/jspiders/swiggy/configuration/UserConfiguration.java
+++ backend/src/main/java/com/jspiders/swiggy/configuration/UserConfiguration.java
@@ -22,10 +22,12 @@ public class UserConfiguration {
 				registry.addMapping("/**")
 					.allowedOrigins(
 							"http://127.0.0.1:5500",
-							"http://localhost:5500"
+							"http://localhost:5500",
+							"https://swiggy-clone-enterprise.vercel.app"
 					)
 					.allowedMethods("GET","PUT","POST","DELETE")
-					.allowedHeaders("*");
+					.allowedHeaders("*")
+					.allowCredentials(true);
 			}
 		};
 	}
